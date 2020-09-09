db0a192083d04c641cd79d46d9f2d9257a69d8ae
nolagx <arnon.h@campus.technion.ac.il>
2020-09-09 10:11:32 +0300

    * query: moved "?" * we now parse the whole tree * switched parser to 'earley' * rules: removed "rule" terminal * minor changes to clear up the tree

--- src/rgxlog-interpreter/src/rgxlog/magic/rgxlog_magic.py
+++ src/rgxlog-interpreter/src/rgxlog/magic/rgxlog_magic.py
@@ -6,6 +6,7 @@ from IPython.core.magic import register_cell_magic
 
 @v_args(inline=True)
 class TestTransformer(Transformer):
+    # get rid of "integer" in the tree
     integer = int
 
 
@@ -14,11 +15,16 @@ class TestTransformer(Transformer):
 def spanner(line, cell):
     grammar_path = os.path.dirname(rgxlog.grammar.__file__)
     with open(f'{grammar_path}/grammar.lark', 'r') as grammar:
-        parser = Lark(grammar, parser='lalr', transformer=TestTransformer(), debug=True)
+        parser = Lark(grammar, parser='earley', debug=False)
+        parse_tree = parser.parse(cell)
+        parse_tree = TestTransformer().transform(parse_tree)
+        print(parse_tree.pretty())
+        print(parse_tree)
+        # leaving this commented for now as it might be useful for debugging
+        # non_empty_lines = (line for line in cell.splitlines() if len(line))
+        #
+        # for line in non_empty_lines:
+        #     print(parser.parse(line).pretty())
+        #     print(parser.parse(line))
+        #     print('-----------------------------------')
 
-        non_empty_lines = (line for line in cell.splitlines() if len(line))
-
-        for line in non_empty_lines:
-            print(parser.parse(line).pretty())
-            print(parser.parse(line))
-            print('-----------------------------------')
