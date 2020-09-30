7d1a52daba3184839dcc7423842ac798ac039711
nDanielGH <n.daniel@campus.technion.ac.il>
2020-09-30 20:15:36 +0300

    all system-wide configuration should be in this file. do not use this file to communicate between functions! (except for reading values written to it during initialization)

--- src/rgxlog-interpreter/src/rgxlog/__init__.py
+++ src/rgxlog-interpreter/src/rgxlog/__init__.py
@@ -1,2 +1,11 @@
-import rgxlog.magic.rgxlog_magic
+try:
+    import rgxlog.magic.rgxlog_magic
+
+    pass
+except NameError:
+    # when the remote server imports system_configuration.py it has to run this
+    # file when this happens, rgxlog_magic throws this error because it has to
+    # be ran from a jupyter notebook and a remote server runs standalone
+    pass
+
 import rgxlog.engine
---
+++ src/rgxlog-interpreter/src/rgxlog/system_configuration.py
@@ -0,0 +1,23 @@
+import os
+
+import rgxlog
+
+# do not use this file to communicate between functions (except for initialization)
+
+system_configuration = {
+    'default_local_client_config': {
+        'user_config_filename': 'local_client_user_config',
+        'user_config_path': f'{os.path.dirname(os.path.abspath(rgxlog.__file__))}/server',
+        'connection_retries': 3,
+        'retry_sleep': 2,
+        'ip': 'localhost',
+        'port': 7945
+    },
+
+    'default_remote_listener_config': {
+        'user_config_filename': 'remote_listener_user_config',
+        'user_config_path': f'{os.path.dirname(os.path.abspath(rgxlog.__file__))}/server',
+        'ip': 'localhost',
+        'port': 7945
+    },
+}
