29559c9deedde8ab6293485c60b256dd0bdbddd4
tom347 <tomfeldman2002@gmail.com>
2021-03-31 11:49:58 +0300

    changed IEFunction design to support both regular and super users

--- src/rgxlog-interpreter/src/rgxlog/engine/ie_functions/ie_function_base.py
+++ src/rgxlog-interpreter/src/rgxlog/engine/ie_functions/ie_function_base.py
@@ -1,39 +1,25 @@
-
-"""
-Instead of creating a separate class for every user defined ie function we will adjust IEFunctionData class.
-Every abstract method will become a class member.
-It allows us to use this class for all ie functions by passing different parameters to the init method.
-"""
-
-"""
-    class IEFunctionData:
-        def __init__(self, ie_function : callable(), get_input_types : callable(), get_output_types : callable()):
-            pass
-"""
-
-
-
-
-"""
-this module contains the 'IEFunctionData' class: an abstraction for classes that contain all the
-information needed to semantic check and execute an information extraction function
-"""
-
-from abc import abstractmethod, ABC
-
-
-class IEFunctionData(ABC):
+class IEFunction:
     """
     A class that contains all the functions that provide data
     needed for using a single information extraction function
     """
 
-    def __init__(self):
-        super().__init__()
+    """
+    Members: 
+        ie_function_def : callable() - the user defined ie function implementation. 
+        in_types        : iter()     - iterable of the input types to the function. 
+        is_super_user   : bool       - false if output arity is constant otherwise true. 
+        out_types       :            - if is_super_user then it's a function that gets output arity and 
+                                       returns iterable of the output types. 
+                                       else, it's iterable of the output types.
+    """
+    def __init__(self, ie_function_def, in_types, out_types, is_super_user: bool):
+        self.ie_function_def   = ie_function_def
+        self.in_types          = in_types
+        self.is_super_user = is_super_user
+        self.out_types         = out_types
 
-    @staticmethod
-    @abstractmethod
-    def ie_function(*args):
+    def ie_function(self, *args):
         """
         The actual information extraction function that will be used
         the function must return a list of lists/tuples that represents the results, another option is to yield the
@@ -44,31 +30,29 @@ class IEFunctionData(ABC):
         an integer should be returned as an int instance
         a span could be returned either as a tuple of length 2, or as a datatypes.Span instance
         """
-        pass
+        return self.ie_function_def(*args)
 
-    @staticmethod
-    @abstractmethod
-    def get_input_types():
+    def get_input_types(self):
         """
         returns an iterable of the input types to the function
         This function must be defined as it is used for type checking in semantic passes and execution.
         """
-        pass
+        return self.in_types
 
-    @staticmethod
-    @abstractmethod
-    def get_output_types(output_arity):
+    def get_output_types(self, output_arity=-1):
         """
         given an expected output arity returns an iterable of the output types to the function.
         if the ie function cannot return an output of length output_arity, should return None.
         This function must be defined as it is used for type checking in semantic passes and execution.
         """
-        """
-        TODO instead of making the user implement this function, allow the user to use a regular expression
-        to define this function. The semantic pass will use said regex and the expected output arity to get the
-        expected output types.
-        for example for the RGX ie function, the output types regex will be spn*.
-        Then, for the call RGX(s1,s2)->(X,Y,Z,W), the semantic pass / execution will use the regex spn* to determine
-        the expected output types to be [spn,spn,spn,spn]
-        """
-        pass
+
+        if self.is_super_user:
+            if output_arity == -1:
+                raise Exception("Missing output arity param.")
+            return self.out_types(output_arity)
+
+        # self.is_super_user = false;
+        if output_arity != -1:
+            raise Exception("Output arity is constant, no need for arity_output param.")
+        return self.out_types
+
