6355fb88334647561bd7af7f74a409151a52592d
tom347 <tomfeldman2002@gmail.com>
2021-04-07 17:28:57 +0300

    changed test

--- src/rgxlog-interpreter/tests/test_introduction_tutorial.py
+++ src/rgxlog-interpreter/tests/test_introduction_tutorial.py
@@ -10,6 +10,6 @@ EXPECTED_RESULT_INTRO = """printing results for query 'uncle(X, Y)':
 # TODO add tests
 def test_introduction():
     session = Session()
-    session._run_query("new uncle(str, str)")
-    session._run_query('uncle("bob", "greg")')
+    session.run_query("new uncle(str, str)")
+    session.run_query('uncle("bob", "greg")')
     query_result = session.execute("?uncle(X,Y)")
