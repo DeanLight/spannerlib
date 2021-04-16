e97ae66235e3c6f4ba956fd870f80d8b0e6c9674
NNrepos <nivimano@gmail.com>
2021-04-16 21:33:39 +0300

    fixed relative paths for tests

--- src/rgxlog-interpreter/tests/test_export_import.py
+++ src/rgxlog-interpreter/tests/test_export_import.py
@@ -1,4 +1,5 @@
 import os
+from os.path import dirname, abspath
 
 import pytest
 from pandas import DataFrame
@@ -6,11 +7,12 @@ from rgxlog import Session
 # csv filenames
 from rgxlog.engine.session import query_to_string
 
-LONGREL_CSV = "query_longrel.csv"
-REL_CSV = "query_rel.csv"
-EXAMPLE_RELATION_CSV = "example_relation.csv"
-EXAMPLE_RELATION_TWO_CSV = "example_relation_two.csv"
-TEMP_CSV = "temp.csv"
+TESTS_DIR = dirname(abspath(__file__))
+LONGREL_CSV = os.path.join(TESTS_DIR, "query_longrel.csv")
+REL_CSV = os.path.join(TESTS_DIR, "query_rel.csv")
+EXAMPLE_RELATION_CSV = os.path.join(TESTS_DIR, "example_relation.csv")
+EXAMPLE_RELATION_TWO_CSV = os.path.join(TESTS_DIR, "example_relation_two.csv")
+TEMP_CSV = os.path.join(TESTS_DIR, "temp.csv")
 
 
 @pytest.fixture(scope="module")
@@ -33,7 +35,7 @@ def test_import_csv1(im_ex_session: Session):
    aoi    |  [0, 3)  |   8
 """
 
-    query_result = im_ex_session.run_query("?csv_rel(X,Y,Z)")
+    query_result = im_ex_session.run_query("?csv_rel(X,Y,Z)", print_results=False)
     query_result_string = query_to_string(*query_result)
     assert query_result_string == expected_result_string
 
@@ -51,7 +53,7 @@ def test_import_csv2(im_ex_session: Session):
   a
 """
 
-    query_result = im_ex_session.run_query("?csv_rel2(X)")
+    query_result = im_ex_session.run_query("?csv_rel2(X)", print_results=False)
     query_result_string = query_to_string(*query_result)
     assert query_result_string == expected_result_string
 
@@ -68,16 +70,16 @@ def test_import_df(im_ex_session: Session):
 """
 
     im_ex_session.import_relation_from_df(df, "df_rel")
-    query_result = im_ex_session.run_query("?df_rel(X)")
+    query_result = im_ex_session.run_query("?df_rel(X)", print_results=False)
     query_result_string = query_to_string(*query_result)
     assert query_result_string == expected_result_string
 
 
 def test_query_into_csv_basic(im_ex_session: Session):
     # create new relation
-    im_ex_session.run_query("new basic_rel(str)")
-    im_ex_session.run_query('basic_rel("stardew")')
-    im_ex_session.run_query('basic_rel("valley")')
+    im_ex_session.run_query("new basic_rel(str)", print_results=False)
+    im_ex_session.run_query('basic_rel("stardew")', print_results=False)
+    im_ex_session.run_query('basic_rel("valley")', print_results=False)
 
     # query into csv and compare with old file
     im_ex_session.query_into_csv('?basic_rel(X)', TEMP_CSV)
@@ -93,10 +95,11 @@ def test_query_into_csv_basic(im_ex_session: Session):
 
 def test_query_into_csv_long(im_ex_session: Session):
     # create new relation
-    im_ex_session.run_query("new longrel(str,spn,int)")  # TODO why not change it into span? much more readable
-    im_ex_session.run_query('longrel("ano sora",[42, 69),24)')
-    im_ex_session.run_query('longrel("aoi",[1, 2),16)')
-    im_ex_session.run_query('longrel("aoi",[0, 3),8)')
+    im_ex_session.run_query("new longrel(str,spn,int)",
+                            print_results=False)  # TODO why not change it into span? much more readable
+    im_ex_session.run_query('longrel("ano sora",[42, 69),24)', print_results=False)
+    im_ex_session.run_query('longrel("aoi",[1, 2),16)', print_results=False)
+    im_ex_session.run_query('longrel("aoi",[0, 3),8)', print_results=False)
 
     # query into csv and compare with old file
     im_ex_session.query_into_csv("?longrel(X,Y,Z)", TEMP_CSV)
@@ -113,9 +116,9 @@ def test_query_into_csv_long(im_ex_session: Session):
 def test_query_into_df(im_ex_session: Session):
     test_df = DataFrame(["king", "jump"], columns=["X"])
     # create new relation
-    im_ex_session.run_query("new df_query_rel(str)")
-    im_ex_session.run_query('df_query_rel("jump")')
-    im_ex_session.run_query('df_query_rel("king")')
+    im_ex_session.run_query("new df_query_rel(str)", print_results=False)
+    im_ex_session.run_query('df_query_rel("jump")', print_results=False)
+    im_ex_session.run_query('df_query_rel("king")', print_results=False)
 
     # query into df and compare
     temp_df = im_ex_session.query_into_df("?df_query_rel(X)")
