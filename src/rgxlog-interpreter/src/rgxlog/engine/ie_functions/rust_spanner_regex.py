cc9ec6ab477f9da17263e20e21826ce35f7080ef
NNrepos <nivimano@gmail.com>
2021-04-20 11:14:36 +0300

    refactored function registration and started working on rust regex

--- src/rgxlog-interpreter/src/rgxlog/engine/ie_functions/rust_spanner_regex.py
+++ src/rgxlog-interpreter/src/rgxlog/engine/ie_functions/rust_spanner_regex.py
@@ -1,14 +1,24 @@
 """
-this module contains implementation of regex ie functions using python's 're' module
+this module contains implementation of regex ie functions using the rust package `enum-spanner-rs`
 """
-
+import logging
 import re
 
 from rgxlog.engine.datatypes.primitive_types import DataTypes
 from rgxlog.engine.ie_functions.ie_function_base import IEFunction
+from os import path
 
-PYTHON_RGX_IN_TYPES = [DataTypes.string, DataTypes.string]
+RUST_RGX_IN_TYPES = [DataTypes.string, DataTypes.string]
 
+PACKAGE_NAME = "enum-spanner-rs"
+
+
+def _download_and_install_regex_package():
+    logging.info(f"{PACKAGE_NAME} was not found on your system. downloading...")
+
+
+def _is_installed_package():
+    return path.isdir()
 
 def rgx_out_type(output_arity):
     return tuple([DataTypes.span] * output_arity)
@@ -22,6 +32,9 @@ def rgx_span(text, regex_pattern):
 
     Returns: tuples of spans that represents the results
     """
+    if not _is_installed_package():
+        _download_and_install_regex_package()
+
     compiled_rgx = re.compile(regex_pattern)
     num_groups = compiled_rgx.groups
     for match in re.finditer(compiled_rgx, text):
@@ -40,6 +53,9 @@ def rgx_string(text, regex_pattern):
 
     Returns: tuples of strings that represents the results
     """
+    if not _is_installed_package():
+        _download_and_install_regex_package()
+
     compiled_rgx = re.compile(regex_pattern)
     num_groups = compiled_rgx.groups
     for match in re.finditer(compiled_rgx, text):
@@ -50,21 +66,21 @@ def rgx_string(text, regex_pattern):
         yield matched_strings
 
 
-class PyRGXSpan(IEFunction):
+class RustRGXSpan(IEFunction):
     """
     Performs a regex information extraction.
     Results are tuples of spans
     """
 
     def __init__(self):
-        super().__init__(rgx_span, PYTHON_RGX_IN_TYPES, rgx_out_type)
+        super().__init__(rgx_span, RUST_RGX_IN_TYPES, rgx_out_type)
 
 
-class PyRGXString(IEFunction):
+class RustRGXString(IEFunction):
     """
     Performs a regex information extraction.
     Results are tuples of strings
     """
 
     def __init__(self):
-        super().__init__(rgx_string, PYTHON_RGX_IN_TYPES, rgx_out_type)
+        super().__init__(rgx_string, RUST_RGX_IN_TYPES, rgx_out_type)
