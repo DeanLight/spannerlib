4ce6d112e5e66d791884c49fb98c32a36d22160d
tom347 <tomfeldman2002@gmail.com>
2021-10-02 21:25:09 +0300

    pep8 fix

--- src/rgxlog-interpreter/src/rgxlog/engine/passes/optimizations_passes.py
+++ src/rgxlog-interpreter/src/rgxlog/engine/passes/optimizations_passes.py
@@ -243,6 +243,7 @@ class PruneUnnecessaryProjectNodes(GenericPass):
 
         return union_to_project_children
 
+
 class RemoveUselessRelationsFromRule(GenericPass):
     """
     This pass removes duplicated relations from a rule.
