96bdffe7f0fd73f21d2aaf1f2a35d2170313016b
DorSH <doratomer@gmail.com>
2022-09-21 11:30:51 +0300

    fix issue 80

--- src/rgxlog-interpreter/tests/test_introduction_tutorial.py
+++ src/rgxlog-interpreter/tests/test_introduction_tutorial.py
@@ -270,8 +270,8 @@ def test_issue_80() -> None:
                 80 |      8 |     2
                 42 |     20 |     5
         """
-
-    run_test(commands, expected_result, ([multiple_highest_2], [multiple_by_2_the_highest]))
+    iefunctions_list = (multiple_highest_2, multiple_by_2_the_highest)
+    run_test(commands, expected_result, iefunctions_list)
 
 
 def test_neq() -> None:
