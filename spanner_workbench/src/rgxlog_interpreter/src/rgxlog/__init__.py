3b0e69e4825621516bf8d82b9b4de9592d191c9a
loayshaqir1 <loayshaqir0@gmail.com>
2023-09-21 03:17:54 +0300

    fix path of Session in __init__ file -> make it relative

--- spanner_workbench/src/rgxlog_interpreter/src/rgxlog/__init__.py
+++ spanner_workbench/src/rgxlog_interpreter/src/rgxlog/__init__.py
@@ -1,7 +1,7 @@
 from typing import Any
 
-from spanner_workbench.src.rgxlog_interpreter.src.rgxlog.engine.session import Session
-from spanner_workbench.src.rgxlog_interpreter.src.rgxlog.magic.rgxlog_magic import RgxlogMagic
+from .engine.session import Session
+from .magic.rgxlog_magic import RgxlogMagic
 from IPython import InteractiveShell, get_ipython
 
 magic_session = Session()
