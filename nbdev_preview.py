99b49fd84deebf68321ebd1a4b6e92f9e6f8ade4
loayshaqir1 <loayshaqir0@gmail.com>
2023-10-23 23:25:48 +0300

    monkey patch filtered procs

--- nbdev_preview.py
+++ nbdev_preview.py
@@ -2,7 +2,7 @@ import inspect
 from nbdev.processors import FilterDefaults
 from nbdev.quarto import nbdev_docs
 
-new_FilterDefaults = """
+monkey_patch_procs = """
 all_procs = FilterDefaults().procs()
 def new_procs(self):
     procs = [proc for proc in all_procs if proc.__name__ != 'clean_magics']
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     module_file_path = inspect.getfile(FilterDefaults)
 
     with open(module_file_path, 'a') as f:
-        f.write(new_FilterDefaults)
+        f.write(monkey_patch_procs)
 
     nbdev_docs()
