d2acf0ecac24573c7a29f841d012ec05b013fd9d
Ofer Katz <ofer.katz@campus.technion.ac.il>
2025-01-22 04:40:59 +0200

    wip

--- nbdev_test.py
+++ nbdev_test.py
@@ -4,7 +4,6 @@ from nbdev.doclinks import nbglob
 
 files_to_test = nbglob()
 files_to_skip = ['extended_version.ipynb']
-processes = []
 
 for file in files_to_test:
     if os.path.basename(file) in files_to_skip:
@@ -13,13 +12,10 @@ for file in files_to_test:
     try:
         # Redirect stderr to stdout
         process = subprocess.Popen(command, shell=True, stderr=subprocess.STDOUT)
-        processes.append(process)
+        process.wait()
     except subprocess.CalledProcessError as e:
         # An error occurred in the subprocess
         print(f"Error occurred while testing {file}: {e}")
-
-# Wait for all subprocesses to finish
-for process in processes:
-    process.wait()
+    
 
 
